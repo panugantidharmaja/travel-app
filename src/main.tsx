ee0a09cab44d1cce1dcc49e4ea378e102bf63d9f
Venkata Panuganti <VenkataKalyaniBalaDharmaja.Panuganti@charter.com>
2025-07-25 18:09:02 -0500

    launch route

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import DestinationDetail from './DestinationDetail/DestinationDetail.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
+        <Route path='/' element={<Navigate to='/home' />}></Route>
         <Route path='/home' element={<App/>}></Route>
         <Route path="/destinationDetail/:id" element={<DestinationDetail />} />  
       </Routes>
